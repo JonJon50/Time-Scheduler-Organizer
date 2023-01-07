8b7b96b6dffd0ceaefeb036ac53e637d3bccc258
John Hagens <ginuwine104@gmail.com>
2023-01-07 13:33:10 -0500

    color blocks

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ console.log("log = ", userlog);
  
  });   
 });
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -36,6 +37,25 @@ console.log("log = ", userlog);
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  function colorCoder(){
+    let currentTime = dayjs().hour();
+
+    $(".time-block").each(function(){
+      let logTime = parseInt($(this).appr("id").split("-")[1]);
+
+      if(logTime < currentTime){
+        $(this).addClass("past");
+      } else if (logTime === currentTime){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  colorCoder();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
