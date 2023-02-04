b750da7af65b0ed67e6d64d2fcf66df6184ad67e
John Hagens <ginuwine104@gmail.com>
2023-02-04 02:55:40 -0500

    clean up code

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function() {
-// Day.js be used to get the
+
   var today = dayjs();
   $("#currentDay").text(today.format("MMM D, YYYY"));
-
+  
   $(".saveBtn").on("click",function(params) {
-    
     console.log(this);
   var id = $(this).attr("id");
+  const currentDayEl = document.getElementById("currentDay");
+  const now = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  currentDayEl.innerHTML = now.toLocaleDateString("en-US", options);
 
-// Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements.
+  
 const userlog = $(this).siblings(".description").val();
 const time = $(this).parent().attr("id");
 console.log("log = ", userlog);
-  
-// Local Storage
-// Appling DOM traversal to be used to get the "hour-x" id of the
-// saving the description in local storage?
-  localStorage.setItem(time, userlog);
-// save the users information to local storage
+
+localStorage.setItem(time, userlog);
+
  });   
 });
 
+$("#clearBtn").on("click", function() {
+    $(".description").val("");
+  });  
+
+function getLocalStorageValues() {
+  const hourIds = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  hourIds.forEach(hourId => {
+    $(`#hour-${hourId} .description`).val(localStorage.getItem(`hour-${hourId}`));
+  });
+}
+
+$(function() {
+  // other code...
+
+  // Call the function to get values from local storage
+  getLocalStorageValues();
+});
+
+ $(function() {
+    getLocalStorageValues();
+  });
   
-  // Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. 
-  // Add code to display the current date in the header of the page.
+
   function colorCoder(){
     let currentTime = dayjs().hour();
 console.log("currentTime = ", currentTime);
-    $(".time-block").each(function(){
-       // Using the id attribute of each time-block be used to conditionally add or remove the
-      // past, present, and future classes on refresh
+       $(".time-block").each(function(){
+    
       let logTime = parseInt($(this).attr("id").split("-")[1]);
       console.log("logTime = ", logTime);
      if(logTime < currentTime){
@@ -50,9 +69,7 @@ console.log("currentTime = ", currentTime);
     });
   }
   colorCoder();
-  // Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements.Id
-  // attribute of each time-block be used to do this?
+ 
   
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
@@ -63,3 +80,5 @@ console.log("currentTime = ", currentTime);
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+ 
